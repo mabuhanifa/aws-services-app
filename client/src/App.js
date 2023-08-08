e78d1f29090b99dc00a13957257dc9fdd86832d3
shourovahmd <moh.abuhanifa@gmail.com>
2023-08-08 23:04:25 +0600

    nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Foods from "./components/Foods";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="p-10 2xl:px-40 bg-black/[.035]">
+    <div className="px-10 2xl:px-40 bg-black/[.035]">
+      <Nav />
       <Foods />
     </div>
   );
---
+++ client/src/components/Nav.js
@@ -0,0 +1,35 @@
+import { CiUser } from "react-icons/ci";
+import { MdFoodBank } from "react-icons/md";
+import { RiSearchLine } from "react-icons/ri";
+
+export default function Nav() {
+  return (
+    <>
+      <nav className="bg-white flex flex-col-reverse gap-y-5 shad rounded-lg py-3 px-5 lg:flex-row justify-between items-center my-10">
+        <div className="lg:flex items-center justify-between hidden">
+          <span className="font-bold">
+            <MdFoodBank className="text-gray-500 inline text-2xl mb-1 mr-2" />
+            Food Finder
+          </span>
+        </div>
+        <div className="flex flex-col gap-y-5 lg:flex-row items-center">
+          <div className="relative">
+            <input
+              type="text"
+              className="bg-gray-200 rounded-md py-2 md:w-96 px-10"
+              placeholder="Search"
+              //   onChange={(e) => {dispatch(addFilter(e.target.value))}}
+            />
+            <RiSearchLine className="text-gray-400 text-2xl absolute left-2 top-2" />
+          </div>
+        </div>
+        <div>
+          <button>
+            <CiUser className="text-2xl text-gray-600 mt-1.5" />
+          </button>
+        </div>
+      </nav>
+      <div></div>
+    </>
+  );
+}
