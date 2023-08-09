2de061e76aae5497484bb5d9d83b0c1f8b414f89
shourovahmd <moh.abuhanifa@gmail.com>
2023-08-10 02:11:51 +0600

    cloudinary

--- server/controllers/foodControllers.js
+++ server/controllers/foodControllers.js
@@ -1,6 +1,7 @@
 const foods = require("../data.json");
 const Food = require("../models/Food");
 
+
 const getAllFoods = async (req, res) => {
   try {
     res.json(foods);
@@ -10,12 +11,37 @@ const getAllFoods = async (req, res) => {
 };
 
 const createFood = async (req, res) => {
-  try {
-    const newFood = Food.create(req.body);
+  const {
+    name,
+    country,
+    price,
+    restaurantName,
+    restaurantAddress,
+    isAvailable,
+  } = req.body;
+  const image = req.files.image;
 
-    res.json(newFood);
+  try {
+    const imageResult = await cloudinary.uploader.upload(image.tempFilePath);
+
+    const newFood = new Food({
+      name,
+      country,
+      img: imageResult.secure_url,
+      price,
+      restaurant: {
+        name: restaurantName,
+        address: restaurantAddress,
+      },
+      isAvailable,
+    });
+
+    await newFood.save();
+
+    res.json({ message: "Food item created successfully", newFood });
   } catch (error) {
-    console.log(error);
+    console.error("Food creation error:", error);
+    return res.status(500).json({ message: "Food creation failed" });
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
@@ -16,6 +17,11 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 app.use("/api/foods", foodRouter);
 
